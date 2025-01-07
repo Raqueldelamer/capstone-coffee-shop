077a5aa8f5c81aa9b63aac9dbed1592b86a76528
Raquel Marie <raqueldelamer@gmail.com>
2025-01-06 22:21:16 -0600

    update daisy theming

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import TestButton from "@/components/TestButton";
 export default function Home() {
   return <h1>My Coffee Shop Frontend Splash Page</h1>;
   <>
-  <NavBar/>
-  <TestButton/>
+  <NavBar />
+  <TestButton />
   </>
 }
