222d64fab26191d335856004e1a1c2cd3f070369
Raquel Marie <raqueldelamer@gmail.com>
2025-01-09 14:25:33 -0600

    update signup form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -4,9 +4,9 @@ import Button from '@/components/Button';
 export default function SignupForm({ buttonLabel }) {
   return (
     <form className="form text-yellow-500 mt-5 text-center font-sans text-l space-x-6 justify-around">
-      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="User Name" />
       <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Create Password" />
       <Button label="Submit" handleClick={()=>{console.log("clicked sign up")}}/>
     </form>
   );
