ed78bb42931fa1128f9c79a91e3a7cfca01e9dc5
Raquel Marie <raqueldelamer@gmail.com>
2025-01-16 01:06:17 -0600

    set up React Query provider in  _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const queryClient = new QueryClient();
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
