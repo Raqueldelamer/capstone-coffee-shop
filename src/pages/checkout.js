e490717e7c2785df385bad919ee95c0f39e73ed5
Raquel Marie <raqueldelamer@gmail.com>
2025-01-19 13:03:21 -0600

    add loading products icon to products/index.js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -30,11 +30,11 @@ export default function CheckoutPage () {
         <>
             
             <Navbar menuItems={["HOME", "LOGIN", "PRODUCTS", "CART", "CHECKOUT"]} />
-            <div style={divStyle}>
-            <h1 className="text-black text-5xl bg-slate-400 opacity-70 drop-shadow-2xl mb-5 mx-auto font-mono font-bold 
-                text-stroke-thick flex justify-around items-center w-full"> CHECK OUT FORM</h1>
-                <div className="flex sm:flex-shrink justify-evenly w-full">
-            <CheckoutForm className="w-full" handleCheckout={handleCheckout}  />
+            <div className="h-screen w-full" style={divStyle}>
+            <h1 className="text-black text-5xl bg-slate-400 opacity-70 drop-shadow-2xl mb-5 font-mono font-bold 
+                text-stroke-thick flex justify-center items-center w-full"> CHECK OUT FORM</h1>
+                <div className="flex justify-evenly w-full">
+            <CheckoutForm className="w-full sm:w-1/2 mb-6" handleCheckout={handleCheckout}  />
                 <div className=" rounded-lg shadow-lg">
             <CartSummary subtotal={subtotal} tax={tax} total={total} />
             </div>
