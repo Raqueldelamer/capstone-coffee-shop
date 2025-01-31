fe15f5fa4a1910331b9a61cf84682ba3534b8b23
Raquel Marie <raqueldelamer@gmail.com>
2025-01-31 00:39:16 -0600

    update readme, about & products

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -48,24 +48,26 @@ export default function ProductsPage() {
     }
 
         if(loading) {
-            return  <div className='text-xl ml-30 mb-10 mt-10 font-mono font-bold 
-            text-stroke-thick justify-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" 
-                    width={32} 
-                    height={32} 
-                    viewBox="0 0 24 24">
-                    <circle cx={18} cy={12} r={0} fill="#d0ad42">
-                    <animate attributeName="r" begin={0.67} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate>
-                    </circle>
-                    <circle cx={12} cy={12} r={0} fill="#d0ad42">
-                    <animate attributeName="r" begin={0.33} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate>
-                    </circle>
-                    <circle cx={6} cy={12} r={0} fill="#d0ad42">
-                    <animate attributeName="r" begin={0} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate>
-                    </circle>
-                </svg>
-                <p className="text-yellow-200 font-mono">Loading products..</p>
-            </div>;
+            return  (
+                <div className='text-xl ml-30 mb-10 mt-10 font-mono font-bold 
+                text-stroke-thick justify-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        width={32} 
+                        height={32} 
+                        viewBox="0 0 24 24">
+                        <circle cx={18} cy={12} r={0} fill="#d0ad42">
+                        <animate attributeName="r" begin={0.67} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate>
+                        </circle>
+                        <circle cx={12} cy={12} r={0} fill="#d0ad42">
+                        <animate attributeName="r" begin={0.33} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate>
+                        </circle>
+                        <circle cx={6} cy={12} r={0} fill="#d0ad42">
+                        <animate attributeName="r" begin={0} calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"></animate>
+                        </circle>
+                    </svg>
+                    <p className="text-yellow-200 font-mono">Loading products..</p>
+                </div>
+            );
         }
         if(error) {
             return <div>Error loading products: {error.message}</div>;
